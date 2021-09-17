b73b76b01c5d9072393867e16ec97ffd9608dc80
Joshua Pagan <joshuajpagan@gmail.com>
2021-09-17 17:39:28 -0400

    Set up Webpack Dev Server
    - added a dev server for react app development
    - created a test Square component to play around with
      ideas for the system map

---
+++ client/components/Square.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+const Square = () => {
+  return (
+    <button className="active" style={css} >nice</button>
+  )
+}
+
+const css = {
+  border: "1px solid black"
+
+}
+
+export default Square
